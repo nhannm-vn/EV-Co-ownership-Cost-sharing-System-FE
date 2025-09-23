0dbdc68b9811eb21de8a3fbe97e30bacb98faab6
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-09-23 12:46:48 +0700

    complete setup project

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,21 @@ export default defineConfig([
     plugins: {
       prettier: eslintPluginPrettier,
     },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          semi: false,
+          trailingComma: "none",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true,
+        },
+      ],
+    },
   },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  ignores: ["vite.config.ts"],
-});
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
+})
