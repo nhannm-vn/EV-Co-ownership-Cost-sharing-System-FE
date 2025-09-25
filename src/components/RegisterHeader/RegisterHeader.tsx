25e9ca743600071f3ec4fa4517ba0bd3a4e57ebd
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-09-25 12:34:08 +0700

    #11 NamNguyen RegisterHeader

--- src/components/RegisterHeader/RegisterHeader.tsx
+++ src/components/RegisterHeader/RegisterHeader.tsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router'
+
 function RegisterHeader() {
-  return <div>RegisterHeader</div>
+  return (
+    <header className='bg-[#0000] sticky top-0 z-50'>
+      <div className='flex justify-between items-center px-6'>
+        <div className='flex w-14 h-14 items-center '>
+          <img src='src\assets\RegisterLogo.png' alt='logo' className='block w-full h-full object-contain' />
+          <span className='ml-2 text-lg font-semibold text-black text-[14px]'>EVShare</span>
+        </div>
+
+        <div className='flex  justify-between items-center'>
+          <Link to='/' className='text-[#1CC29F] px-6 py-2 rounded'>
+            Login
+          </Link>
+          <Link to='/' className='text-[#fff] font-semibold bg-[#1CC29F] w-32 text-[14px] px-6 py-2 rounded'>
+            Registration
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default RegisterHeader
