865288091fc9ba34ffcfd7ecc2415009fd92a898
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-09-25 14:39:11 +0700

    #18 Nhan Nguyen fix code

--- src/components/RegisterHeader/RegisterHeader.tsx
+++ src/components/RegisterHeader/RegisterHeader.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router'
 
 function RegisterHeader() {
   return (
-    <header className='bg-white top-0 z-50 p-3 overflow-x-auto'>
+    <header className='bg-white top-0 z-50 p-1 overflow-x-auto'>
       <div className='flex justify-between items-center px-6'>
-        <div className='flex w-16 h-16 items-center mr-24'>
+        <div className='flex w-24 h-24 items-center mr-24'>
           <img src='src\assets\RegisterLogo.png' alt='logo' className='block w-full h-full object-contain' />
-          <span className='ml-2 text-lg font-semibold text-black text-[14px]'>EVShare</span>
+          <div className='ml-2 text-lg font-semibold text-black text-[14px]'>EVShare</div>
         </div>
 
         <div className='flex  justify-between items-center'>
