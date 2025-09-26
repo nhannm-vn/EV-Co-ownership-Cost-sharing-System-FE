f261663edbda451b0986f4e431874a4e9cb9c719
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-09-26 12:22:06 +0700

    #40 Nam Nguyen create Home About

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import HomeAnimation from './components/HomeAnimation'
 import HomeSlide from './components/HomeSlide'
 import HomeService from './components/HomeService'
 import HomeLine from './components/HomeLine'
+import HomeAbout from './components/HomeAbout'
 
 function Home() {
   return (
     <Fragment>
       <HomeAnimation />
       <HomeService />
+      <HomeAbout />
       <HomeLine />
       <HomeSlide />
     </Fragment>
