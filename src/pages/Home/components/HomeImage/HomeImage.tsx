2437e552d641f02ccc4f5956725f49eaa75127e4
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-09-26 13:08:09 +0700

    #41 Nhan Nguyen code fixed background

---
+++ src/pages/Home/components/HomeImage/HomeImage.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  image: string
+  content: string
+}
+
+export default function HomeImage({ image, content }: Props) {
+  return (
+    <section
+      className='h-[750px] bg-cover bg-center bg-fixed flex items-center justify-center'
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <h2 className='text-4xl font-bold text-white drop-shadow-lg'>{content}</h2>
+    </section>
+  )
+}
