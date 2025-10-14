b2878d6b9bf893ee310a945294873b4de10ef81e
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-10-14 21:29:09 +0700

    #146 Nam Nguyen config aside group Page

--- src/pages/GroupPage/pages/InviteMember/InviteMember.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function InviteMember() {
-  return <div>InviteMember</div>
-}
--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -17,13 +17,13 @@ import Login from './pages/Login'
 import MyAccount from './pages/MyAccount'
 import NotFound from './pages/NotFound'
 
+import CoOwnershipPercentage from './pages/GroupPage/pages/Co-ownershipPercentage/CoOwnershipPercentage'
+import DashboardGP from './pages/GroupPage/pages/DashboardGP'
+import OwnershipRatio from './pages/GroupPage/pages/OwnershipRatio'
 import Register from './pages/Register'
 import OTPInput from './pages/UI-OTP'
 import UploadLicense from './pages/UploadLicense'
 import Viewgroups from './pages/Viewgroups'
-import OwnershipRatio from './pages/GroupPage/pages/OwnershipRatio'
-import InviteMember from './pages/GroupPage/pages/InviteMember/InviteMember'
-import DashboardGP from './pages/GroupPage/pages/DashboardGP'
 
 function useRouteElements() {
   const routeElements = useRoutes([
@@ -146,7 +146,7 @@ function useRouteElements() {
             { index: true, element: <DashboardGP /> },
             { path: 'createContract', element: <CreateContract /> },
             { path: 'viewMembers', element: <MemberGroup /> },
-            { path: 'inviteMembers', element: <InviteMember /> },
+            { path: 'ownershipPercentage', element: <CoOwnershipPercentage /> },
             { path: 'ownershipRatio', element: <OwnershipRatio /> }
           ]
         }
