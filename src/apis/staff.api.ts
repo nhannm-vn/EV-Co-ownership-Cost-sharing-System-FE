6b794ca3b922078a20a8e170844f4de099978a3d
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-10-20 16:53:54 +0700

    #210 Nhan Nguyen - fix

---
+++ src/apis/staff.api.ts
@@ -0,0 +1,24 @@
+import type { UserOfStaff } from '../types/api/staff.type'
+import { getAccessTokenFromLS } from '../utils/auth'
+import http from '../utils/http'
+
+const accessToken = getAccessTokenFromLS()
+
+const staffApi = {
+  getUsers() {
+    return http.get<UserOfStaff>('api/staff/users', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  },
+  getUsersPendingLicense() {
+    return http.get<UserOfStaff>('api/staff/documents/pending', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  }
+}
+
+export default staffApi
