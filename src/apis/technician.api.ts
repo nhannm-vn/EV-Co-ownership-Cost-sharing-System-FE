6c3782c351937f32da9704c9021a8a1fba838ea6
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-11-04 18:51:47 +0700

    #340 Nhan Nguyen - code

--- src/apis/technician.api.ts
+++ src/apis/technician.api.ts
@@ -4,6 +4,11 @@ import http from '../utils/http'
 const technicianApi = {
   getAllVehicleCheck() {
     return http.get<BookingReviewResponse>('/api/vehicle-checks')
+  },
+  checkReport(checkId: string, status: 'APPROVED' | 'REJECTED') {
+    return http.put(`/api/vehicle-checks/${checkId}/status`, {
+      status: status
+    })
   }
 }
 
