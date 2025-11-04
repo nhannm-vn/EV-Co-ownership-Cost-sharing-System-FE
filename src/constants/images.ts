35c7dd4d55db4eb32c57d8c428c1ed057a974b94
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-11-04 14:17:48 +0700

    config url image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,24 +1,24 @@
 export const LOGO_URL = {
-  white: 'https://evsharestorage.blob.core.windows.net/images/z7141589149138_de308edc1e1d5e40f1ffa86a9476c547.jpg',
-  black: 'https://evsharestorage.blob.core.windows.net/images/d0bc1c54-6d26-403a-a365-fd7130644e2b_1761103177179.jpg'
+  white: 'https://evshare.blob.core.windows.net/images/z7141589149138_de308edc1e1d5e40f1ffa86a9476c547.jpg',
+  black: 'https://evshare.blob.core.windows.net/images/d0bc1c54-6d26-403a-a365-fd7130644e2b_1761103177179.jpg'
 }
 
 export const REGISTER_IMG_URL = 'http://128.199.169.153/images/z7152798850521_0b21841a1c686f4578af0bf2c68dbb8b.jpg'
 
 export const LOGIN_IMG_URL =
-  'https://evsharestorage.blob.core.windows.net/images/z7141541067364_15cf7bc1689c0ad6fc892fc8ce86b3fa.jpg'
+  'https://evshare.blob.core.windows.net/images/z7141541067364_15cf7bc1689c0ad6fc892fc8ce86b3fa.jpg'
 
 export const BACKGROUNDFIXED_IMG_URL = {
-  first: 'https://evsharestorage.blob.core.windows.net/images/z7141543487285_37cff5e2299be908f664383e52538709.jpg',
-  second: 'https://evsharestorage.blob.core.windows.net/images/z7141542226525_5d6a5bcadfe61d7303b74e569d421895.jpg'
+  first: 'https://evshare.blob.core.windows.net/images/z7141543487285_37cff5e2299be908f664383e52538709.jpg',
+  second: 'https://evshare.blob.core.windows.net/images/z7141542226525_5d6a5bcadfe61d7303b74e569d421895.jpg'
 }
 
 export const SLIDES_IMG_URL = [
-  'https://evsharestorage.blob.core.windows.net/images/z7141543520823_70bc7888e3730695d7d3be2544fafb93.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543401435_a19e9a340bf2bb260d848d92c2e8609a.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543364000_9be80a35bc6082e3519319176564f94d.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543321611_b704bc3a8544e73c4788662a07fdeb1c.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543271868_478addab02d39844a4b0bad7a9ea4b97.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543182122_a4199060ecc9a71e9700e9c5b3cb13ee.jpg',
-  'https://evsharestorage.blob.core.windows.net/images/z7141543108797_f1e085afca7ad885d4fee935ef9c45a5.jpg'
+  'https://evshare.blob.core.windows.net/images/z7141543520823_70bc7888e3730695d7d3be2544fafb93.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543401435_a19e9a340bf2bb260d848d92c2e8609a.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543364000_9be80a35bc6082e3519319176564f94d.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543321611_b704bc3a8544e73c4788662a07fdeb1c.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543271868_478addab02d39844a4b0bad7a9ea4b97.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543182122_a4199060ecc9a71e9700e9c5b3cb13ee.jpg',
+  'https://evshare.blob.core.windows.net/images/z7141543108797_f1e085afca7ad885d4fee935ef9c45a5.jpg'
 ]
